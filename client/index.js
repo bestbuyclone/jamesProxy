c823434b5f9c9eff93193eec72c848995df3e88b
James Dempsey <james.dempsey420@gmail.com>
2019-02-19 16:50:48 -0600

    i hate the dist folder

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from "React";
+import ReactDOM from "react-dom";
+import App from "./app";
+import CSS from "../styles.css";
+
+ReactDOM.render(<App />, document.getElementById("app"));
