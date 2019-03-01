af8d831730855e905e1699489912da76ab129ec2
James Dempsey <james.dempsey420@gmail.com>
2019-03-01 16:26:59 -0600

    style and postioning, as well as functionality

--- client/app.js
+++ client/app.js
@@ -8,6 +8,18 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <header id="header">
+          <nav>
+            <ul>
+              <li>Weekly Ad</li>
+              <li>Deal of the Day</li>
+              <li>Credit Card</li>
+              <li>For Your Buisness</li>
+              <li>Gift Cards</li>
+              <li>Gift Ideas</li>
+            </ul>
+          </nav>
+        </header>
         <h1>This is the Best Buy Clone Page</h1>
       </div>
     );
